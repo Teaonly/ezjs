3a4f97c9cd4543f3e1d6d0a7fdf3fad6173148e2
Zhou Chang <achang.zhou@gmail.com>
2021-05-06 15:48:59 +0800

    clean code for update test_debug

--- tests/test_debug.js
+++ tests/test_debug.js
@@ -1,13 +1,21 @@
 // Simple debug script
 
-var OneClass = function() {
-    this.x = "haha";
+function check_dims( dims ) {
+    var n = dims.length;
+    var check = [];
+    for (var i = 0; i < n; i++) {
+        if (dims[i] < 0 || dims[i] >= n) {
+            throw new Error("Error: out of dimensition!");
+        }
+        
+        for (var j = 0; j < check.length; j++) {
+            if (check[j] == dims[i]) {
+                throw new Error("Error: repeated dimensition!");
+            }
+        }
+        check.push( dims[i] );
+    }
+    return true;
 }
-OneClass.prototype.test = function(y) {
-    console.log(y);
-    console.log(this.x);
-    return this.x;
-};
 
-var a = new OneClass();
-a.test.call(a, 3.14);
+check_dims( [1,2,5,0] );
